4ace08ceced063dddb19d92a4222d09012c9ecf9
aadaam <aadaam@1f34a7e8-5d51-0410-a500-d176386315cc>
2008-07-07 19:14:00 +0000

    Added copyright

--- LazyTemplateEngine.class.php
+++ LazyTemplateEngine.class.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * PHP OEmbed provider/proxy - for details, visit
+ *
+ * http://code.google.com/p/php-oembed/
+ *
+ * Copyright(C) by Adam Nemeth. Licensed under New BSD license.
+ *
+ * I would love to hear every feedback on aadaam at googlesmailservice
+ *
+ */
 require_once("config.php");
 class LazyTemplateEngine {
   private $_scriptPath=TEMPLATE_PATH;//comes from config.php
--- config.php
+++ config.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * PHP OEmbed provider/proxy - for details, visit
+ *
+ * http://code.google.com/p/php-oembed/
+ *
+ * Copyright(C) by Adam Nemeth. Licensed under New BSD license.
+ *
+ * I would love to hear every feedback on aadaam at googlesmailservice
+ *
+ */
 define("PROVIDER_XML","providers.xml");
 define("TEMPLATE_PATH","templates/");
 function __autoload($classname){
